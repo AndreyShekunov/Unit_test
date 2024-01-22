a6bf752b5b0f59ec1cf5b9af86edb7c66ef817a8
Andrey Shekunov <ashekunov@gmail.com>
2024-01-22 19:29:07 +0500

    Домашняя работа 6

--- untitest/src/main/java/hw6/ListCompare.py
+++ untitest/src/main/java/hw6/ListCompare.py
@@ -1,28 +1,29 @@
+def calculate_average(input_list):
+    """
+    Рассчитать среднее значение списка.
+
+    :param input_list: Список чисел.
+    :return: Среднее значение.
+    """
+    if len(input_list) == 0:
+        return 0
+    return sum(input_list) / len(input_list)
+
+
 class ListCompare:
     """
     Этот класс сравнивает средние значения двух списков.
     """
 
-    def __init__(self, list1, list2):
+    def __init__(self, list_one, list_two):
         """
         Конструктор класса.
 
-        :param list1: Первый список чисел.
-        :param list2: Второй список чисел.
+        :param list_one: Первый список чисел.
+        :param list_two: Второй список чисел.
         """
-        self.list1 = list1
-        self.list2 = list2
-
-    def calculate_average(self, input_list):
-        """
-        Рассчитать среднее значение списка.
-
-        :param input_list: Список чисел.
-        :return: Среднее значение.
-        """
-        if len(input_list) == 0:
-            return 0
-        return sum(input_list) / len(input_list)
+        self.list_one = list_one
+        self.list_two = list_two
 
     def compare_lists(self):
         """
@@ -30,11 +31,11 @@ class ListCompare:
 
         :return: Сообщение, указывающее, какой список имеет большее среднее значение.
         """
-        average_one = self.calculate_average(self.list1)
-        average_two = self.calculate_average(self.list2)
+        average_one = calculate_average(self.list_one)
+        average_two = calculate_average(self.list_two)
 
         if average_one > average_two:
             return "Первый список имеет большее среднее значение"
-        if average_one > average_two:
+        if average_one < average_two:
             return "Второй список имеет большее среднее значение"
         return "Средние значения равны"
